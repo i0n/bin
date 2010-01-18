569e51f41492cabb12325e28327feebeb148b3cc
i0n <ianalexanderwood@gmail.com>
2010-01-18 13:55:35 +0000

    added templates folder and changed structure to incorporate templates for sinatra and other frameworks and project types

---
+++ templates/rails.rb
@@ -0,0 +1,195 @@
+################################################################### SET INSTALL VARIABLES #################################
+
+# Get the name of the app
+app_name = `pwd`.split('/').last.strip
+
+# Get the name or IP of the remote server
+puts "Enter the name or IP address of the remote server for this project"
+remote_server = gets.chomp
+
+# Sets the path to the rails-templates repository
+rails_templates_path = '/Users/i0n/sites/bin/templates'
+
+################################################################### METHODS #############################################
+
+# Method for inserting a line after a specific line in a file. Note that this method will match only the first line it finds.
+def append_after_line(target_file, target_line, new_line)
+  file = IO.readlines(target_file)
+  new_file = file.dup
+  file.each do |line|
+    if line.chomp == target_line.chomp && new_line != nil
+      new_file.insert((file.find_index(line) + 1), new_line)
+      new_line = nil
+    end
+  end
+  File.open(target_file, "w") do |x|
+    x.puts new_file
+  end
+end
+
+# Method for appending text to the end of a file
+def append_to_end_of_file(target_file,new_line,remove_last_line)
+  file = IO.readlines(target_file)
+  file.pop if remove_last_line == true
+  file.push new_line
+  File.open(target_file, "w") do |x|
+    x.puts file
+  end
+end
+
+# Add app to local Nginx config so that it can be accessed through a browser
+# Wrapped nginx.conf actions in a method so it can be reused with other parameters for remote servers
+def nginx_auto_config(target_file,nginx_new)
+  nginx_config = IO.readlines(target_file)
+  nginx_last_line = nginx_config.pop
+  nginx_config.push nginx_new
+  nginx_config.push nginx_last_line
+  File.open(target_file, "w") do |file|
+    file.puts nginx_config
+  end  
+end
+
+################################################################### INSTALL PLUGINS #####################################
+
+# Install Exception Notification plugin
+plugin 'exception_notification', :git =>  'git://github.com/rails/exception_notification.git'
+# Add recipiant email addresses to production.rb environment so that exception_notification plugin knows where to send exceptions
+append_to_end_of_file('config/environments/production.rb', "# Config for email addresses to send exceptions to \nExceptionNotifier.exception_recipients = \%w(ianalexanderwood@gmail.com)", false)
+# adds the include for exception_notifiable to application controller
+append_after_line('app/controllers/application_controller.rb', 'class ApplicationController < ActionController::Base', '  include ExceptionNotifiable')
+
+################################################################### INSTALL GEMS #####################################
+
+# Setup RSpec to use gem(s)
+run "echo '\nconfig.gem \"rspec\", :lib => false, :version => \">= 1.2.0\"\nconfig.gem \"rspec-rails\", :lib => false, :version => \">= 1.2.0\"'>>config/environments/test.rb"
+
+# Install Haml/Sass and config Sass to compress generated stylesheets
+run "haml --rails ."
+run "echo '\n\nSass::Plugin.options[:style] = :compressed' >> config/environment.rb"
+
+# Install Gems if they are not already on the local system
+rake("gems:install", :sudo => true)
+
+# Freeze Gems for upload
+rake("rails:freeze:gems")
+rake("gems:unpack")
+
+################################################################### CREATE DATABASES AND RUN MIGRATIONS #####################################
+
+# create databases
+rake("db:create:all")
+
+# Migrate database (included here in case of the adition of migrations at a later date)
+rake("db:migrate")
+
+# Generate rspec files
+generate("rspec")
+rake("spec")
+
+################################################################### CREATE AND EDIT FILES ##################################################
+
+# Create .gitignore
+file ".gitignore" do
+ IO.read("#{rails_templates_path}base/.gitignore")
+end
+
+# Change Production environment config for use with remote server
+append_to_end_of_file('config/database.yml', '  socket: /var/run/mysqld/mysqld.sock', true)
+
+# config for local OSX Nginx environment
+nginx_auto_config('/usr/local/nginx/conf/nginx.conf', "
+   server {
+     listen 80;
+     server_name #{app_name}.local;
+     rails_env development;
+     client_max_body_size 10M;
+     client_body_buffer_size 128k;
+     location / {
+       passenger_enabled on;
+       root /Users/i0n/Sites/#{app_name}/public;
+     }
+   }")
+
+# Appending URI to /etc/hosts to complete local OSX Nginx config
+run "echo '\n127.0.0.1 #{app_name}.local'>>/etc/hosts"
+
+# Create HAML application layout
+file 'app/views/layouts/application.html.haml' do
+  IO.read("#{rails_templates_path}/rails/app/views/layouts/application.html.haml")
+end
+
+# Create stylesheets and sass directories and populate with standard stylesheet framework
+Dir.mkdir 'public/stylesheets/sass'
+file 'public/stylesheets/layout.css' do
+  IO.read("#{rails_templates_path}/base/public/stylesheets/layout.css")
+end
+file 'public/stylesheets/sass/global.sass' do
+  IO.read("#{rails_templates_path}/base/public/stylesheets/sass/global.sass")
+end
+file 'public/stylesheets/sass/ie.sass' do
+  IO.read("#{rails_templates_path}/base/public/stylesheets/sass/ie.sass")
+end
+file 'public/stylesheets/sass/mobile.sass' do
+  IO.read("#{rails_templates_path}/base/public/stylesheets/sass/mobile.sass")
+end
+file 'public/stylesheets/sass/print.sass' do
+  IO.read("#{rails_templates_path}/base/public/stylesheets/sass/print.sass")
+end
+file 'public/stylesheets/sass/screen.sass' do
+  IO.read("#{rails_templates_path}/base/public/stylesheets/sass/screen.sass")
+end
+
+# Create JQuery core file
+file 'public/javascripts/jquery-1.3.2.min.js' do
+  IO.read("#{rails_templates_path}/base/public/javascripts/jquery-1.3.2.min.js")
+end
+# Create JQuery document ready file
+file 'public/javascripts/init.js' do
+  IO.read("#{rails_templates_path}/base/public/javascripts/init.js")
+end
+
+# Set the logger for production environment to split log file on a daily basis
+append_to_end_of_file('config/environments/production.rb', "# Config logger \nconfig.logger = Logger.new(config.log_path, 'daily')", false)
+
+################################################################### CAPISTRANO CONFIGURATION #############################################################
+
+# Set application up for deployment with Capistrano
+capify!
+
+# Read Capistrano template and replace remote_server and app_name variables with the values entered during app creation
+cap_txt = IO.read("#{rails_templates_path}/rails/config/deploy.rb")
+cap_txt.gsub!(/\#\{remote_server\}/, "#{remote_server}")
+cap_txt.gsub!(/\#\{app_name\}/, "#{app_name}")
+file 'config/deploy.rb' do
+  cap_txt
+end
+
+################################################################### GIT CONFIGURATION #############################################################
+
+# Initialise Git repository
+git :init
+
+# Add all changes to the Git repository and then commit those changes.
+git :add => "."
+git :commit => "-v -a -m 'Initial commit'"
+
+################################################################### REMOTE DEPLOYMENT ##############################################################
+
+# Ask about deploying to remote server
+if yes?("Do you want to push this project to the remote server #{remote_server}?")
+  puts "Deploying to: #{remote_server}"
+  run "ssh #{remote_server} sudo git clone git://github.com/i0n/dotfiles.git /usr/local/bin/dotfiles" 
+  run "ssh #{remote_server} sudo cp /usr/local/bin/dotfiles/make_git_repo /usr/local/bin/make_git_repo"
+  run "ssh #{remote_server} sudo cp /usr/local/bin/dotfiles/nginx_auto_config /usr/local/bin/nginx_auto_config"
+  run "ssh #{remote_server} sudo rm -rf /usr/local/bin/dotfiles"
+  run "ssh #{remote_server} 'make_git_repo #{app_name}'"
+  run "git remote add origin ssh://#{remote_server}/~/git/#{app_name}.git"
+  run "git push origin master"
+  run "cap deploy:setup"
+  run "cap deploy:create_mysql_db"
+  run "cap deploy:check"
+  run "cap deploy:nginx"
+  run "cap deploy:migrations"
+end
+
+puts "SUCCESS!"
---
+++ templates/rails/config/deploy.rb
@@ -0,0 +1,71 @@
+set :user, 'i0n'
+set :domain, "#{remote_server}"
+set :application, "#{app_name}"
+
+set :repository,  "#{user}@#{domain}:/home/#{user}/git/#{application}.git"
+
+#default_environment['PATH']='/usr/local/bin /usr/local/sbin /usr/local/mysql/bin /usr/local/bin /usr/local/sbin /usr/local/mysql/bin /usr/local/bin /usr/local/sbin /usr/local/mysql/bin /usr/local/bin /usr/local/sbin /usr/local/mysql/bin /usr/local/bin /usr/local/sbin /usr/local/mysql/bin /usr/local/bin /usr/local/sbin /usr/local/mysql/bin /usr/local/bin /usr/local/sbin /usr/local/mysql/bin /usr/bin /bin /usr/sbin /sbin /usr/local/bin /usr/local/git/bin /usr/X11/bin /Users/i0n/sites/bin /Users/i0n/sites/bin /Users/i0n/sites/bin /Users/i0n/sites/bin /Users/i0n/sites/bin /Users/i0n/sites/bin /Users/i0n/sites/bin'
+#default_environment['GEM_PATH']='/usr/lib/ruby/gems/1.8'
+
+role :app, domain                           # This may be the same as the `Web` server
+role :web, domain                           # Your HTTP server, Apache/etc
+role :db,  domain , :primary => true        # This is where Rails migrations will run
+
+set :scm_verbose, true
+
+set :scm, :git
+set :scm_username, user
+set :runner, user
+set :use_sudo, true
+set :branch, "master"
+set :deploy_via, :checkout
+set :git_shallow_clone, 1
+set :deploy_to, "/home/#{user}/sites/#{application}"
+default_run_options[:pty] = true
+
+namespace :deploy do
+  #task which causes Passenger to initiate a restart
+  task  :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  task :install_gems do
+    run "cd #{current_path}; rake gems:install"
+  end
+  
+  task :create_mysql_db do
+    run "mysqladmin -u root create #{application}_production"
+  end
+  
+  #Task to set up the remote Nginx server for app deployment
+  task :nginx do
+    run "#{sudo} nginx_auto_config '/opt/nginx/conf/nginx.conf' \"
+    server {
+      listen 80;
+      server_name #{application}.com;
+      gzip on;
+      gzip_proxied any;
+      gzip_vary on;
+      gzip_disable 'MSIE [1-6]\\.';
+      gzip_http_version 1.1;
+      gzip_min_length 10;
+      gzip_comp_level 9;
+      gzip_types text/plain application/xhtml+xml text/css application/x-javascript text/xml application/xml;
+      rails_env production;
+      location / {
+        root /home/i0n/sites/#{application}/current/public/;
+        passenger_enabled on;
+        if (\\$request_filename ~ '.+\\.(jpg|jpeg|gif|css|png|js|ico|html)$') { access_log off; expires 30d; }
+      }
+    }
+    \"
+    "
+  end
+end
+
+after 'deploy:update_code', 'deploy:install_gems'
+
+# Reminder of default actions for cap deploy:
+# deploy:update_code
+# deploy:symlink
+# deploy:restart
