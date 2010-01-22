9b241ac1368fdec8c1bc2882cdf5c2e60dc226fd
i0n <ianalexanderwood@gmail.com>
2010-01-22 12:41:00 +0000

    refined nginx_config script

--- nginx_auto_config.rb
+++ nginx_auto_config.rb
@@ -1,11 +1,22 @@
 def nginx_auto_config(source_file, target_file, app_name)
   nginx_config = IO.readlines(target_file)
-  nginx_last_line = nginx_config.pop
+  nginx_config.delete_if do |line|
+    line == "\n" || line == ""
+  end
+  nginx_config.reverse_each do |line|
+    if line == "}\n" || line == "}" && @nginx_last_line == nil
+      @nginx_last_line = nginx_config.slice!(nginx_config.find_index(line))
+    end
+  end
   source_config = IO.read(source_file)
   nginx_config.push source_config
-  nginx_config.push nginx_last_line
+  nginx_config.push @nginx_last_line
   nginx_config.each {|line| line.gsub!(/\#\{app_name\}/, "#{app_name}")}
   File.open(target_file, "w") do |file|
     file.puts nginx_config
-  end  
+  end
+  puts "Success! Nginx config for #{app_name} has been added to #{target_file}"
+  puts
+  puts "******************************************************************************************************************************************"
+  puts
 end
