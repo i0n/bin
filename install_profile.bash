05910db9e5ad2480e3b31a9f5a2bdb8db2b031f6
Ian Alexander Wood <ianalexanderwood@gmail.com>
2022-10-05 22:00:31 +0100

    updates

--- install_profile.bash
+++ install_profile.bash
@@ -6,5 +6,4 @@
 rm -R -f ~/.bash_profile ~/.bash* ~/.profile ~/.zsh* ~/.ssh/config ~/.vim* ~/.gvim* && ln -s ~/bin/dotfiles/zsh/zshrc ~/.zshrc && ln -s ~/bin/dotfiles/zsh/env ~/.zshenv && ln -s ~/bin/dotfiles/ssh/config ~/.ssh/config && ln -s ~/bin/dotfiles/vim/vimrc ~/.vimrc && ln -s ~/bin/dotfiles/vim ~/.vim && ln -s ~/bin/dotfiles/bash/bashrc ~/.bashrc && ln -s ~/bin/dotfiles/vim/gvimrc ~/.gvimrc && mkdir ~/.vimtmp
 
 rm -rf ~/.config/nvim && ln -s ~/bin/dotfiles/nvim ~/.config/nvim
-
-mkdir dotfiles/nvim/lua/user && ln -s dotfiles/nvim_AstroNvim/user/init.lua dotfiles/nvim/lua/user/init.lua
+ln -s ~/bin/dotfiles/nvim_AstroNvim/user ~/bin/dotfiles/nvim/lua/user
