26897f8df34d205fbaf98a3e45db660bb11de807
Ian Alexander Wood <ianalexanderwood@gmail.com>
2023-04-08 11:27:01 +0100

    turn off relative line numbering. turn on spelling.

--- dotfiles/nvim_AstroNvim/user/options.lua
+++ dotfiles/nvim_AstroNvim/user/options.lua
@@ -2,9 +2,9 @@
 return {
   opt = {
     -- set to true or false etc.
-    relativenumber = true, -- sets vim.opt.relativenumber
+    relativenumber = false, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
+    spell = true, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
   },
