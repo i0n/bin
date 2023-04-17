61bd6285fb6d5e7ca0938917307ba9adc992b703
Ian Alexander Wood <ianalexanderwood@gmail.com>
2023-04-17 11:01:44 +0100

    clean up a bit

--- install_profile.bash
+++ install_profile.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Removes existing bash zsh ssh and vim dotfiles and sets up links to ~/bin/dotfiles
-# Creates .vimtmp directory for Vim swap files
 
-rm -R -f ~/.bash_profile ~/.bash* ~/.profile ~/.zsh* ~/.ssh/config ~/.vim* ~/.gvim* && ln -s ~/bin/dotfiles/zsh/zshrc ~/.zshrc && ln -s ~/bin/dotfiles/zsh/env ~/.zshenv && ln -s ~/bin/dotfiles/ssh/config ~/.ssh/config && ln -s ~/bin/dotfiles/vim/vimrc ~/.vimrc && ln -s ~/bin/dotfiles/vim ~/.vim && ln -s ~/bin/dotfiles/bash/bashrc ~/.bashrc && ln -s ~/bin/dotfiles/vim/gvimrc ~/.gvimrc && mkdir ~/.vimtmp
+rm -R -f ~/.bash_profile ~/.bash* ~/.profile ~/.zsh* ~/.ssh/config ~/.vim* ~/.gvim* && ln -s ~/bin/dotfiles/zsh/zshrc ~/.zshrc && ln -s ~/bin/dotfiles/zsh/env ~/.zshenv && ln -s ~/bin/dotfiles/ssh/config ~/.ssh/config
 
 rm -rf ~/.config/nvim && ln -s ~/bin/dotfiles/nvim ~/.config/nvim
 ln -s ~/bin/dotfiles/nvim_AstroNvim/user ~/bin/dotfiles/nvim/lua/user
