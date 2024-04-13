faa47fd285813b1e749a4105ab37f2339362c0ab
i0n <ianalexanderwood@gmail.com>
2024-04-13 20:02:57 +0100

    update AstroNvim to latest version

--- install_profile.bash
+++ install_profile.bash
@@ -5,4 +5,3 @@
 rm -R -f ~/.bash_profile ~/.bash* ~/.profile ~/.zsh* ~/.ssh/config ~/.vim* ~/.gvim* && ln -s ~/bin/dotfiles/zsh/zshrc ~/.zshrc && ln -s ~/bin/dotfiles/zsh/env ~/.zshenv && ln -s ~/bin/dotfiles/ssh/config ~/.ssh/config
 
 rm -rf ~/.config/nvim && ln -s ~/bin/dotfiles/nvim ~/.config/nvim
-ln -s ~/bin/dotfiles/nvim_AstroNvim/user ~/bin/dotfiles/nvim/lua/user
